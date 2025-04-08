31cb6dfd3966637097f8a906ae084c8168b44535
fadel <fadel.seydou@gmail.com>
2025-04-08 20:24:22 +0200

    update

---
+++ fraudetect/config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Arguments:
+
+
+    # data pre-processing
+    
+
+    # training parameters
+    max_epochs:int
+    learning_rate:float
+    weightdecay:float
+
+    # data augmentation
+
+
+
